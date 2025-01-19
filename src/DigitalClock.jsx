0643f2792712bdfe39f52982d4fe2f19cb6f23e1
Ahmed Hisham <ahmedhisham1928@gmail.com>
2025-01-19 23:38:42 +0200

    Doneee

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,14 +1,32 @@
 import { useState , useEffect } from "react";
 
 function DigitalClock(){
-    const [time , settime] = useState()
+    const [time , settime] = useState(new Date())
+
+    useEffect(() => {
+        const interval = setInterval(() =>{
+            settime(new Date())
+        }
+        ,1000)
+
+        return() => clearInterval(interval)
+    } ,[])
+
+    function formattime(){
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+        let ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12 || 12;
+        return `${hours}:${minutes}:${seconds} ${ampm}`
+    }
 
 
     return(
         <>
         <div className="clock-container">
             <div className="clock">
-                <span>012:21:21</span>
+                <span>{formattime()}</span>
             </div>
         </div>
         
