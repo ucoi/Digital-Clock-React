68e77e6bde7c9ffceb9677c2f863870fcc9b7107
Ahmed Hisham <ahmedhisham1928@gmail.com>
2025-01-19 23:27:53 +0200

    made the component

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,4 +1,18 @@
 import { useState , useEffect } from "react";
 
-function DigitalClock(){}
+function DigitalClock(){
+    const [time , settime] = useState()
+
+
+    return(
+        <>
+        <div className="clock-container">
+            <div className="clock">
+                <span>012:21:21</span>
+            </div>
+        </div>
+        
+        </>
+    );
+}
 export default DigitalClock;
